b6222b68e746e1b1c8c92c6be3baf22138d2cc22
Rokas <rookas.rudzenskas@gmail.com>
2021-07-22 17:10:33 +0300

    Rokas

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import Video from "react-native-video";
+import * as React from 'react';
+import styles from "./styles.js";
+import { View, StyleSheet, Button } from 'react-native';
+import { Video, AVPlaybackStatus } from 'expo-av';
 
 const Post = () => {
+    const video = React.useRef(null);
+    const [status, setStatus] = React.useState({});
     return (
-        <View>
-           <Video source={{ uri: "https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4"}} />
+        <View style={styles.container}>
+            <Video
+                ref={video}
+                style={styles.video}
+                source={{
+                    uri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+                }}
+                useNativeControls
+                resizeMode="contain"
+                isLooping
+                onPlaybackStatusUpdate={status => setStatus(() => status)}
+            />
+            <View style={styles.buttons}>
+                <Button
+                    title={status.isPlaying ? 'Pause' : 'Play'}
+                    onPress={() =>
+                        status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+                    }
+                />
+            </View>
         </View>
     );
-};
+}
 
 export default Post;
---
+++ src/components/Post/styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      width: '100%',
+      height: 400,
+    },
+    video: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    }
+});
+
+export default styles;

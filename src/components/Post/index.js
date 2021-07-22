1dc155856edc98b8c91730de72da6d46411b0df5
Rokas <rookas.rudzenskas@gmail.com>
2021-07-22 17:15:51 +0300

    Rokas

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -5,28 +5,18 @@ import { Video, AVPlaybackStatus } from 'expo-av';
 
 const Post = () => {
     const video = React.useRef(null);
-    const [status, setStatus] = React.useState({});
     return (
         <View style={styles.container}>
             <Video
                 ref={video}
                 style={styles.video}
+                shouldPlay
                 source={{
                     uri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
                 }}
-                useNativeControls
-                resizeMode="contain"
                 isLooping
-                onPlaybackStatusUpdate={status => setStatus(() => status)}
             />
-            <View style={styles.buttons}>
-                <Button
-                    title={status.isPlaying ? 'Pause' : 'Play'}
-                    onPress={() =>
-                        status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
-                    }
-                />
-            </View>
+
         </View>
     );
 }

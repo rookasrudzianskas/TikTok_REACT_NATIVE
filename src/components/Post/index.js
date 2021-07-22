c6f2a8275f2fec7d5462b98459558f2ae16697da
Rokas <rookas.rudzenskas@gmail.com>
2021-07-22 16:53:22 +0300

    Rokas

---
+++ src/components/Post/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Post = () => {
+    return (
+        <View>
+            <Text>Camera</Text>
+        </View>
+    );
+};
+
+export default Post;

80f8fcf857ff5092ddbd62435e7400b42e0d2c23
Rokas <rookas.rudzenskas@gmail.com>
2021-07-23 11:49:21 +0300

    Rokas

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -29,6 +29,7 @@ const styles = StyleSheet.create({
         padding: 10,
         flexDirection: 'row',
         justifyContent: 'space-between',
+        alignItems: 'flex-end',
     },
     handle: {
         color: "#fff",
@@ -57,8 +58,8 @@ const styles = StyleSheet.create({
       width: 50,
       height: 50,
       borderRadius: 25,
-      borderWidth: 2,
-      borderColor: "#fff",
+      borderWidth: 5,
+      borderColor: "#4c4c4c",
     },
     // right container goes in here
     profilePictureContainer: {

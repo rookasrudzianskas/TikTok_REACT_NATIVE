d1e42ea22bf177bdd71abfac52866e1f1299eb0d
Rokas <rookas.rudzenskas@gmail.com>
2021-07-23 12:48:28 +0300

    Rokas

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import Post from "../../components/Post";
+import posts from "../../data/posts";
 
 
-const post = {
-    id: 'p1',
-    videoUri: 'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
-    user: {
-        id: 'u1',
-        username: 'rookas',
-        imageUri: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg',
-    },
-    description: 'hahahah oh boy @rookas',
-    songName: 'NF - The search',
-    songImage: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg',
-    likes: 123,
-    comments: 12,
-    shares: 44,
-}
 
 const Home = () => {
     return (
         <View>
-            <Post post={post} />
+            <FlatList data={posts}  />
         </View>
     )
 }

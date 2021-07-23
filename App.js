e58739f5a6e9ad20a3ce0d72ea5f9c936db1e0b3
Rokas <rookas.rudzenskas@gmail.com>
2021-07-23 13:21:28 +0300

    Rokas

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Home from "./src/screens/Home";
 import "react-native-gesture-handler";
+import RootNavigation from "./src/navigation";
 
 export default function App() {
 
@@ -10,8 +11,7 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-
-      <Home />
+        <RootNavigation />
     </SafeAreaView>
   );
 }

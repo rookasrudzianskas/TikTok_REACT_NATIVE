e4c79b5504510da6139dda2a1a1b4ca1e6dc35e5
Rokas <rookas.rudzenskas@gmail.com>
2021-07-23 14:36:41 +0300

    Rokas

--- App.js
+++ App.js
@@ -4,8 +4,15 @@ import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Home from "./src/screens/Home";
 import "react-native-gesture-handler";
 import RootNavigation from "./src/navigation";
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react-native'
 
-export default function App() {
+
+Amplify.configure(config);
+
+
+function App() {
 
 
   return (
@@ -20,3 +27,6 @@ const styles = StyleSheet.create({
   container: {
   },
 });
+
+export default withAuthenticator(App)
+

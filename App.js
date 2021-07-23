8d1b8d1f2526a844f6a72fa3b647598d233c48df
Rokas <rookas.rudzenskas@gmail.com>
2021-07-23 16:07:16 +0300

    Rokas'

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Home from "./src/screens/Home";
 import "react-native-gesture-handler";
@@ -7,12 +7,24 @@ import RootNavigation from "./src/navigation";
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react-native'
-
+import {Auth, API, graphqlOperation} from "aws-amplify";
 
 Amplify.configure(config);
 
 
 function App() {
+
+  useEffect(() => {
+    const fetchUser = async() => {
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+      // get currently authenticated user
+
+      // check if the user exists in the database, if it does not, meaning it is newly registered user, then please create a new user in the database
+
+    }
+
+    fetchUser();
+  }, []);
 
 
   return (

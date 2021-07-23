1e9ad9dfb3469122bf698a5edbdc05e1f0099f41
Rokas <rookas.rudzenskas@gmail.com>
2021-07-23 13:16:55 +0300

    Rokas

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Home from "./src/screens/Home";
+import "react-native-gesture-handler";
 
 export default function App() {
 

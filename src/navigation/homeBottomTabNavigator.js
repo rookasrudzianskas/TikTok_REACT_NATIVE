10af9ba1126f4bf320aec692d669fdcdf24fc223
Rokas <rookas.rudzenskas@gmail.com>
2021-07-23 13:31:50 +0300

    Rokas

--- src/navigation/homeBottomTabNavigator.js
+++ src/navigation/homeBottomTabNavigator.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {View, Text} from "react-native";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import Home from "../screens/Home";
 
 const Tab = createBottomTabNavigator();
 
 const HomeBottomTabNavigator = () => {
     return (
-        <View>
-
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen name={"Home"} component={Home} />
+            <Tab.Screen name={"Search"} component={Home} />
+            <Tab.Screen name={"Upload"} component={Home} />
+            <Tab.Screen name={"Messages"} component={Home} />
+            <Tab.Screen name={"Inbox"} component={Home} />
+            <Tab.Screen name={"Profile"} component={Home} />
+        </Tab.Navigator>
     );
 };
 

52af97e6ae2cb8ade879e852f66fa2442b00abc9
Rokas <rookas.rudzenskas@gmail.com>
2021-07-23 13:24:20 +0300

    Roaks

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -6,6 +6,7 @@ import posts from "../../data/posts";
 
 
 const Home = () => {
+
     return (
         <View>
             <FlatList showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false} data={posts} renderItem={({item}) => <Post post={item} />} snapToInterval={Dimensions.get('window').height -80} snapToAlignment={"start"} decelerationRate={"fast"}/>

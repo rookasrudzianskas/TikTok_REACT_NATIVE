7862d33ebe3208b5564355496e1f0adb6e4a9699
Rokas <rookas.rudzenskas@gmail.com>
2021-07-23 17:34:29 +0300

    Rokas:

---
+++ src/screens/CreatePost/index.js
@@ -0,0 +1,14 @@
+import { Camera } from 'expo-camera';
+import React, {useEffect, useRef, useState} from 'react';
+import {View, Text, TouchableOpacity} from "react-native";
+import styles from "./styles";
+
+const CreatePost = () => {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+};
+
+export default CreatePost;

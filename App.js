f58bb1a799d93f82e79d3bbc82b4ebce73809c8c
Rokas <rookas.rudzenskas@gmail.com>
2021-07-23 16:43:25 +0300

    Rokas

--- App.js
+++ App.js
@@ -41,57 +41,100 @@ function App() {
   }, []);
 
 
-
-
-  useEffect(() => {
-    const fetchUser = async() => {
-      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
-
-
-      if(!userInfo) {
-        return;
-      }
-
-      const getUserResponse = await API.graphql(graphqlOperation(getUser, {id: userInfo.attributes.sub}));
-
-      if(getUserResponse.data.getUser) {
-        console.log("User already exists in the database 🚀");
-        return;
-      }
-
-      const newUser = {
-        id: userInfo.attributes.sub,
-        username: userInfo.username,
-        email: userInfo.attributes.email,
-        imageUri: getRandomImage(),
-
-      }
-
-
-      // get currently authenticated user
-
-      // check if the user exists in the database, if it does not, meaning it is newly registered user, then please create a new user in the database
-
-      await API.graphql(graphqlOperation(createUser, {input: newUser}));
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
     }
 
-    fetchUser();
-  }, []);
+  //
+  // useEffect(() => {
+  //   const fetchUser = async() => {
+  //     const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+  //
+  //
+  //     if(!userInfo) {
+  //       return;
+  //     }
+  //
+  //     const getUserResponse = await API.graphql(graphqlOperation(getUser, {id: userInfo.attributes.sub}));
+  //
+  //     if(getUserResponse.data.getUser) {
+  //       console.log("User already exists in the database 🚀");
+  //       return;
+  //     }
+  //
+  //     const newUser = {
+  //       id: userInfo.attributes.sub,
+  //       username: userInfo.username,
+  //       email: userInfo.attributes.email,
+  //       imageUri: getRandomImage(),
+  //
+  //     }
+  //
+  //
+  //     // get currently authenticated user
+  //
+  //     // check if the user exists in the database, if it does not, meaning it is newly registered user, then please create a new user in the database
+  //
+  //     await API.graphql(graphqlOperation(createUser, {input: newUser}));
+  //   }
+  //
+  //   fetchUser();
+  // }, []);
 
 
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
-      <StatusBar style="light" />
-        <RootNavigation />
-    </SafeAreaView>
+    // <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
+    //   <StatusBar style="light" />
+    //     <RootNavigation />
+    // </SafeAreaView>
+
+      <View style={styles.container}>
+        <Camera style={styles.camera} type={type}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  setType(
+                      type === Camera.Constants.Type.back
+                          ? Camera.Constants.Type.front
+                          : Camera.Constants.Type.back
+                  );
+                }}>
+              <Text style={styles.text}> Flip </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
 
 
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-  },
+    container: {
+        flex: 1,
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        flexDirection: 'row',
+        margin: 20,
+    },
+    button: {
+        flex: 0.1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 18,
+        color: 'white',
+    },
 });
 
 export default withAuthenticator(App)

312791b7c56f09d84d29b569902fda8a9b20959f
Rokas <rookas.rudzenskas@gmail.com>
2021-07-25 17:30:34 +0300

    ROkas

--- src/screens/CreatePost/index.js
+++ src/screens/CreatePost/index.js
@@ -17,6 +17,7 @@ const CreatePost = () => {
     const navigation = useNavigation();
     const navigation = useNavigation();
     const navigation = useNavigation();
+    const navigation = useNavigation();
 
     const uploadToStorage = async (imagePath) => {
 
